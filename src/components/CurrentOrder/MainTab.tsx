13999a4622e50e0000bb9a6a209ca26063135c7b
jina <pja9362@g.skku.edu>
2024-09-23 05:15:17 +0900

    feat(home): 주문현황 메인 탭 레이아웃 잡기

--- src/components/CurrentOrder/MainTab.tsx
+++ src/components/CurrentOrder/MainTab.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Tab, Tabs, Typography } from "@mui/material";
 
 const MainTab: React.FC = () => {
+    const [tabIndex, setTabIndex] = useState<number>(0);
+
+    const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setTabIndex(newValue);
+    }
+
+    const tabsInfo = [
+        { label: "새로 들어온 주문!", index: 0 },
+        { label: "진행중인 주문", index: 1 },
+        { label: "완료된 주문", index: 2 },
+    ];
+
     return (
         <div>
-        <h1>MainTab</h1>
+            <Tabs
+                value={tabIndex}
+                onChange={handleTabChange}
+                variant="fullWidth"
+                textColor="primary"
+                indicatorColor="primary"
+            >
+                {tabsInfo.map((tab) => (
+                    <Tab key={tab.index} label={tab.label} />
+                ))}
+            </Tabs>
         </div>
     );
 }
